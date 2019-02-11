f38c4013198f72b60b4dad4cd9293a4a3a0667bb
Uladzislau.Barkou <dmkofgl@gmail.com>
2019-02-11 13:57:40 +0300

    Remove Rollback transaction

--- src/test/java/dl/news/portal/domain/service/UserServiceTest.java
+++ src/test/java/dl/news/portal/domain/service/UserServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -72,7 +71,6 @@ public class UserServiceTest {
     }
 
     @Test
-    @Rollback
     public void updateUser() {
         String newUsername = "testTester", newEmail = "sven@qwert.ce";
         UserDto updatedUser = new UserDto(newUsername, newEmail);
